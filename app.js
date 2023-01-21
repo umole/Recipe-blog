8168915cf6f5ea9943fc20131e2754235cfd7227
Umole Andrew <39883080+umole@users.noreply.github.com>
2023-01-22 00:56:28 +0100

    using boostrap

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.use(express.urlencoded({extended: true}));
 app.use(express.static('public'));
 app.use(expressLayouts);
 
-app.set('layouts', './layouts/main');
+app.set('layout', './layouts/main');
 app.set('view engine', 'ejs');
 const routes = require('./server/routes/recipeRoutes.js');
 app.use('/', routes);
