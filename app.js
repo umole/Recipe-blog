d8a610dea81d5661dcf4e34cbff5ec765c2ec4c4
Umole Andrew <39883080+umole@users.noreply.github.com>
2023-01-21 22:33:16 +0100

    Creating a recipie blog site

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
+require('dotenv').config();
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+app.use(express.urlencoded({extended: true}));
+app.use(express.static('public'));
+app.use(expressLayouts);
+
+app.set('layouts', './layouts/main');
+const routes = require('./server/routes/recipeRoutes.js');
+app.use('/', routes);
+
+app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));
